488c53631756f88aced06f983cc9d8011fd609e0
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-06-29 13:19:36 -0400

    [Update] - level 02

---
+++ Assets/03.UI/Scripts/BarrierManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierManager : MonoBehaviour
+{
+    private SliderJoint2D barrier;
+    private JointMotor2D aux;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        barrier = GetComponent<SliderJoint2D>();
+
+        aux = barrier.motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(barrier.limitState == JointLimitState2D.UpperLimit)
+        {
+            aux.motorSpeed = Random.Range(-1, -5);
+            barrier.motor = aux;
+
+        }
+
+        if (barrier.limitState == JointLimitState2D.LowerLimit)
+        {
+            aux.motorSpeed = Random.Range(1, 5);
+            barrier.motor = aux;
+        }
+    }
+}
