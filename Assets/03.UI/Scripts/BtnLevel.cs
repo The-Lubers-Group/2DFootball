b8f163b7a736dbeb4d3b695af111459b06693c71
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2023-07-03 10:56:29 -0400

    [Update] - comentário no script Level Manager 1

---
+++ Assets/03.UI/Scripts/BtnLevel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Criando um objeto BtnLevel para conter as informações básicas do botão
+ */
+
+public class BtnLevel : MonoBehaviour
+{
+    public TMP_Text levelTextBTN;
+    public int unlockBTN;
+}
--- Assets/03.UI/Scripts/LevelManager.cs
+++ Assets/03.UI/Scripts/LevelManager.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
- 
+
+
+/*
+ * LevelManager -- Criar e gerir a tela de seleção de nível
+ */
+
 public class LevelManager : MonoBehaviour
 {
 
+    //Classe que contém as informações básicas do objeto level
     [System.Serializable]public class Level
     {
         public string levelName;
-        public bool enable;
+        public bool isEnable;
         public int unlock;
     }
 
@@ -17,24 +23,26 @@ public class LevelManager : MonoBehaviour
     public Transform localBtn;
     public List<Level> levelsList;
 
+    // Criação de uma lista de botões para mostrar as fases disponíveis
     void ListAdd()
     {
+        // Percorra a lista de fases
         foreach (Level level in levelsList) 
         {
             GameObject newBtn = Instantiate(levelBtn) as GameObject;
+
+            //Instanciando à classe BtnLevel
+            BtnLevel btnNew = newBtn.GetComponent<BtnLevel>();
+            btnNew.levelTextBTN.text = level.levelName;
+            btnNew.unlockBTN = level.unlock;
+            btnNew.GetComponent<Button>().interactable = level.isEnable;
+
             newBtn.transform.SetParent(localBtn, false);
         }
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         ListAdd();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
